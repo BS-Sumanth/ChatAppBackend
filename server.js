f2c68a4902e5ee65df9988480e9b1a2ea20fb92f
BS-Sumanth <21cse033@bnmit.in>
2024-02-20 23:56:51 +0530

    Updated CORS

--- server.js
+++ server.js
@@ -12,7 +12,14 @@ const PORT = process.env.PORT || process.env.API_PORT;
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://chat-app-frontend-rosy.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 
 //app.use('/api/auth', authRoutes);
 //app.use('/api/friend-invitation', friendInvitationRoutes);
